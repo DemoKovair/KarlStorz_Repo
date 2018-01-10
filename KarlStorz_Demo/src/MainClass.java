e974120fb654ebe2140eec0077c1757c8739ce4a
sd261 <sd261@demo1.sr.kovair.com>
2018-01-09 20:35:19 -0800

    First Commit

---
+++ KarlStorz_Demo/src/MainClass.java
@@ -0,0 +1,44 @@
+ 
+class Krishnamurthy
+{
+    // function to calculate the factorial
+    // of any number
+    static int factorial(int n)
+    {
+        int fact = 1;
+        while (n != 0)
+        {
+            fact = fact*n;
+            n--;
+        }
+        return fact;
+    }
+ 
+    static boolean isKrishnamurthy(int n)
+    {
+        int sum = 0;
+ 
+        int temp = n;
+        while (temp != 0)
+        {
+            
+            sum += factorial(temp%10);
+ 
+           
+            temp = temp/10;
+        }
+ 
+        
+        return (sum == n);
+    }
+ 
+    // Driver code
+    public static void main(String[] args)
+    {
+        int n = 145;
+        if (isKrishnamurthy(n))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
